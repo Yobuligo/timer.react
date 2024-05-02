c5f09c5d1426ec725c6a7e6dea8f204119c383ac
Yobuligo <hoffmannpeter82@gmail.com>
2024-05-02 21:57:31 +0200

    Update Time on change

--- src/features/timerItem/TimerItem.tsx
+++ src/features/timerItem/TimerItem.tsx
@@ -1,17 +1,22 @@
 import { useId } from "react";
+import { Sound } from "../../types/Sound";
 import { SoundSelector } from "../soundSelector/SoundSelector";
 import { ITimerItemProps } from "./ITimerItemProps";
 
 export const TimerItem: React.FC<ITimerItemProps> = (props) => {
   const inputId = useId();
 
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.timer.time = parseInt(event.target.value);
+  };
+
   return (
     <div>
       <label htmlFor={inputId}>Time in min</label>
-      <input type="number" id={inputId} min={0} />
+      <input type="number" id={inputId} min={0} onChange={onChange} />
       <SoundSelector
         onSelect={(sound) => {
-          props.timer.sound = sound;
+          props.timer.sound = Sound[sound] as unknown as Sound;
           props.onChange(props.timer);
         }}
       />
--- src/features/timerPanel/TimerPanel.tsx
+++ src/features/timerPanel/TimerPanel.tsx
@@ -1,9 +1,13 @@
 import { ITimerPanelProps } from "./ITimerPanelProps";
 
 export const TimerPanel: React.FC<ITimerPanelProps> = (props) => {
+  const onStart = () => {
+    debugger;
+  };
+
   return (
     <>
-      <button>Start</button>
+      <button onClick={onStart}>Start</button>
       <button>Reset</button>
     </>
   );
