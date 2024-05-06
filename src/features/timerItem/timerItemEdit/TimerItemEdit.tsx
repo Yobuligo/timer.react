19e7aa4d7f20c0380af68f88c3d54e1d3bb5688e
Yobuligo <hoffmannpeter82@gmail.com>
2024-05-06 14:07:39 +0200

    Style TimerItemEdit

--- src/features/timerItem/timerItemEdit/TimerItemEdit.tsx
+++ src/features/timerItem/timerItemEdit/TimerItemEdit.tsx
@@ -1,5 +1,7 @@
 import { ConfirmIcon } from "../../../components/icons/ConfirmIcon";
 import { DeleteIcon } from "../../../components/icons/DeleteIcon";
+import { Sound } from "../../../types/Sound";
+import { SoundSelector } from "../../soundSelector/SoundSelector";
 import { TimerItemCard } from "../timerItemCard/TimerItemCard";
 import { ITimerItemEditProps } from "./ITimerItemEditProps";
 import styles from "./TimerItemEdit.module.scss";
@@ -7,6 +9,11 @@ import styles from "./TimerItemEdit.module.scss";
 export const TimerItemEdit: React.FC<ITimerItemEditProps> = (props) => {
   const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
     props.timerConfig.title = event.target.value;
+    props.onChange(props.timerConfig);
+  };
+
+  const onSelectSound = (sound: Sound) => {
+    props.timerConfig.sound = Sound[sound] as unknown as Sound;
     props.onChange(props.timerConfig);
   };
 
@@ -20,6 +27,10 @@ export const TimerItemEdit: React.FC<ITimerItemEditProps> = (props) => {
       />
       <div className={styles.footer}>
         <input type="time" className={styles.watch} />
+        <SoundSelector
+          initialSound={props.timerConfig.sound}
+          onSelect={onSelectSound}
+        />
         <div>
           <DeleteIcon />
           <ConfirmIcon />
